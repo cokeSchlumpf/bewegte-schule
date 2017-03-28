fb9d0f57ea13cb4478cc598a59e8d6c40bc02633
Michael Wellner <michael.wellner@de.ibm.com>
2017-03-29 00:45:47 +0200

    Added server.

---
+++ server/server.js
@@ -0,0 +1,50 @@
+
+'use strict'
+
+const _ = require('lodash')
+const bodyParser = require('body-parser');
+const chalk = require('chalk')
+const cookieParser = require('cookie-parser');
+const express = require('express')
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const msg = require('gulp-messenger')
+const path = require('path');
+
+const app = express()
+
+// Some default middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// all static content can be routed if its URL is entered explicitly
+app.use(express.static(path.join(__dirname, 'public')));
+
+// for all other routes see /client/src/routes.js
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.set('port', (process.env.PORT || 3000));
+
+// lets startup this puppy
+app.listen(app.get('port'), () => {
+  msg.log('\n')
+  console.log(chalk.cyan('Server Started ' + new Date()));
+  msg.log('\n')
+  const serverInfo = chalk.yellow(`http://localhost:${app.get('port')}`);
+  msg.success('=', _.pad(`Application Running On: ${serverInfo}`, 80), '=')
+});
