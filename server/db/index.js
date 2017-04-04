618b451345ddf6462801db486fd6abb9ffa7dacb
Michael Wellner <michael.wellner@de.ibm.com>
2017-04-04 22:06:45 +0200

    sc.

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,11 @@ const cloudant = require('cloudant');
 
 console.log('Services:');
 console.log(cfenv.getAppEnv());
+console.log(cfenv.getAppEnv(),getServices());
 console.log(cfenv.getAppEnv().getService('cloudantNoSQLDB'));
+
+if (cfenv.getAppEnv().getService('cloudantNoSQLDB')) {
+
+}
 
 module.exports = {};
