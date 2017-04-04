6ef05f6c671893f664ff8d651229afd6bf467abe
Michael Wellner <michael.wellner@de.ibm.com>
2017-04-04 22:19:34 +0200

    Some more changes.

--- server/db/index.js
+++ server/db/index.js
@@ -1,13 +1,19 @@
 const cfenv = require('cfenv');
-const cloudant = require('cloudant');
+const Cloudant = require('cloudant');
+const devdb = require('./local-dev');
 
 console.log('Services:');
 console.log(cfenv.getAppEnv());
-console.log(cfenv.getAppEnv(),getServices());
+console.log(cfenv.getAppEnv().getServices());
 console.log(cfenv.getAppEnv().getService('cloudantNoSQLDB'));
 
-if (cfenv.getAppEnv().getService('cloudantNoSQLDB')) {
+const service = cfenv.getAppEnv().getService('cloudantNoSQLDB') || devdb;
+const url = service[0].credentials.url;
+const username = service[0].credentials.username;
+const password = service[0].credentials.password;
 
-}
+console.log(url);
+console.log(username);
+console.log(password);
 
 module.exports = {};
---
+++ server/db/local-dev.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    'credentials': {
+      'host': '77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix.cloudant.com',
+      'password': 'ca545e74efd103c043ee693c7689391585dc3a3ccf6626b33a4d1e456cccf70d',
+      'port': 443,
+      'url': 'https://77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix:ca545e74efd103c043ee693c7689391585dc3a3ccf6626b33a4d1e456cccf70d@77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix.cloudant.com',
+      'username': '77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix'
+    },
+    'label': 'cloudantNoSQLDB',
+    'name': 'wellnr-node-test-cloudantNoSQLDB',
+    'plan': 'Shared',
+    'provider': null,
+    'syslog_drain_url': null,
+    'tags': [
+      'data_management',
+      'ibm_created',
+      'lite',
+      'ibm_dedicated_public'
+    ]
+  }
+]
