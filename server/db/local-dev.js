c90aaa171638f8cc92095ea481307976d570005a
Michael Wellner <michael.wellner@de.ibm.com>
2017-04-11 22:05:04 +0100

    sc.

--- server/db/local-dev.js
+++ server/db/local-dev.js
@@ -4,7 +4,7 @@ module.exports = [
       'host': '77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix.cloudant.com',
       'password': 'ca545e74efd103c043ee693c7689391585dc3a3ccf6626b33a4d1e456cccf70d',
       'port': 443,
-      'url': 'https://77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix:ca545e74efd103c043ee693c7689391585dc3a3ccf6626b33a4d1e456cccf70d@77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix.cloudant.com',
+      'url': 'https://6cefffcb-81a3-41e9-be86-3503d61a581b-bluemix:585c6265af553b7aef620273c5cbfdee0f435d10594b007153947da890ac3211@6cefffcb-81a3-41e9-be86-3503d61a581b-bluemix.cloudant.com',
       '_url': 'http://admin:pass@localhost:8080',
       'username': '77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix'
     },
