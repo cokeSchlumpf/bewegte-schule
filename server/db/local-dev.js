b734fcd226a57be3c5ad9a38626ccc2d2e74c914
Michael Wellner <michael.wellner@de.ibm.com>
2017-04-11 21:54:27 +0100

    sc.

--- server/db/local-dev.js
+++ server/db/local-dev.js
@@ -4,8 +4,8 @@ module.exports = [
       'host': '77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix.cloudant.com',
       'password': 'ca545e74efd103c043ee693c7689391585dc3a3ccf6626b33a4d1e456cccf70d',
       'port': 443,
-      '_url': 'https://77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix:ca545e74efd103c043ee693c7689391585dc3a3ccf6626b33a4d1e456cccf70d@77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix.cloudant.com',
-      'url': 'http://admin:pass@localhost:8080',
+      'url': 'https://77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix:ca545e74efd103c043ee693c7689391585dc3a3ccf6626b33a4d1e456cccf70d@77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix.cloudant.com',
+      '_url': 'http://admin:pass@localhost:8080',
       'username': '77d05943-d70c-4d72-b100-3696a8ea1e25-bluemix'
     },
     'label': 'cloudantNoSQLDB',
